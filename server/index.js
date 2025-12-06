84c23f866289f539302fd84dc7b9a593739f69fe
nerd-code77 <iamdhiraj.777@gmail.com>
2025-12-06 15:55:28 +0530

    Fixed PORT error

--- server/index.js
+++ server/index.js
@@ -81,4 +81,9 @@ app.delete('/api/posts/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
